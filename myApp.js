4752cfa867a90e50005506853cf926d5244cabc5
jimmy parra <jimmyparra2022@gmail.com>
2023-03-03 19:09:56 -0500

    lesson 3.0.0 serving an html file

--- myApp.js
+++ myApp.js
@@ -4,11 +4,12 @@ let app = express();
 console.log( "Hello World" );
 
 app.get('/', function(req, res) {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
+app.get('/', function(req, res) {
+    res.send('Hello Express');
+});
 
 
 
