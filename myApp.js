5fea76bf437bc1f77ec5d71511e65a6438976c82
jimmy parra <jimmyparra2022@gmail.com>
2023-03-04 21:18:46 -0500

    lesson 7.0.0 create a logger

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
 let express = require('express');
 let app = express();
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use((req, res, next) => {
+    console.log( `${req.method} ${req.path} - ${req.ip}` );
+    next();
+});
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
+
+app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', function(req, res) {
     res.send('Hello Express');
