46b6d853f0fa0a353c09f7090a5ea21e846a2862
jimmy parra <jimmyparra2022@gmail.com>
2023-03-05 03:06:05 -0500

    lesson 8.0.3 create a time server

--- myApp.js
+++ myApp.js
@@ -8,15 +8,15 @@ const middleware = (req, res, next) => {
 
 app.get('/now', middleware, (req, res) => {
     res.send( { time: req.time } );
-})
-
-app.use((req, res, next) => {
-    console.log( `${req.method} ${req.path} - ${req.ip}` );
-    next();
 });
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use((req, res, next) => {
+    console.log( `${req.method} ${req.path} - ${req.ip}` );
+    next();
 });
 
 app.use('/public', express.static(__dirname + '/public'))
