dafb0d78e6f0a16b6f1bee26d624a00ef7e77f5b
jimmy parra <jimmyparra2022@gmail.com>
2023-03-05 20:19:50 -0500

    lesson 10.1 get query parameter input from client

--- myApp.js
+++ myApp.js
@@ -6,44 +6,44 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/name', (req, res) => {
-    const { first: firstname, last: lastname } = req.query;
-    res.json({
-        name: `${firstname} ${lastname}`
-    });
-});
-
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/:word/echo', (req, res) => {
-    const { word } = req.params;
-    res.json({
-        echo: word
-    });
-});
+// app.get('/name', (req, res) => {
+//     const { first: firstname, last: lastname } = req.query;
+//     res.json({
+//         name: `${firstname} ${lastname}`
+//     });
+// });
 
-const middleware = (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-}
+// app.get('/:word/echo', (req, res) => {
+//     const { word } = req.params;
+//     res.json({
+//         echo: word
+//     });
+// });
 
-app.get('/now', middleware, (req, res) => {
-    res.send( { time: req.time } );
-});
+// const middleware = (req, res, next) => {
+//     req.time = new Date().toString();
+//     next();
+// }
 
-app.get('/json', (req, res) => {
-    process.env.MESSAGE_STYLE === "uppercase" 
-        ? res.json({ message: "Hello json".toUpperCase() })
-        : res.json({ message: "Hello json" });
-});
+// app.get('/now', middleware, (req, res) => {
+//     res.send( { time: req.time } );
+// });
 
-app.get('/', function(req, res) {
-    res.send('Hello Express');
-});
+// app.get('/json', (req, res) => {
+//     process.env.MESSAGE_STYLE === "uppercase" 
+//         ? res.json({ message: "Hello json".toUpperCase() })
+//         : res.json({ message: "Hello json" });
+// });
+
+// app.get('/', function(req, res) {
+//     res.send('Hello Express');
+// });
 
 
  module.exports = app;
